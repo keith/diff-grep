c695926ca577ecd964970453f320c251606b0618
Keith Smiley <keithbsmiley@gmail.com>
2026-09-22 12:26:46 -0700

    Update all deps

--- src/opts.rs
+++ src/opts.rs
@@ -1,24 +1,22 @@
-extern crate clap;
-
-#[derive(clap::Clap)]
-#[clap(version = "0.2.0", author = "Keith Smiley <keithbsmiley@gmail.com>")]
+#[derive(clap::Parser)]
+#[command(version = "0.2.0", author = "Keith Smiley <keithbsmiley@gmail.com>")]
 pub struct Opts {
     /// The input file path containing the patch to grep, defaults to stdin
-    #[clap(short, long, default_value = "-")]
+    #[arg(short, long, default_value = "-")]
     pub input: String,
     /// The output file path to write any matches, defaults to stdout
-    #[clap(short, long, default_value = "-")]
+    #[arg(short, long, default_value = "-")]
     pub output: String,
     /// The literal patterns to match against
-    #[clap(required = true)]
+    #[arg(required = true)]
     pub patterns: Vec<String>,
     /// Select lines that do not match any of the patterns
-    #[clap(short = 'v', long)]
+    #[arg(short = 'v', long)]
     pub invert_match: bool,
 }
 
 impl Opts {
     pub fn parse() -> Opts {
-        clap::Clap::parse()
+        clap::Parser::parse()
     }
 }
